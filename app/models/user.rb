e7685081f164f9c1438ce53d66927cfc377881f6
harutoreon <norabder567@gmail.com>
2023-10-15 20:39:58 +0900

    Add User.search method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  def self.search(search, word)
+    case search
+    when 'perfect'
+      User.where('name LIKE?', "#{word}")
+    when 'forward'
+      User.where('name LIKE?', "#{word}%")
+    when 'backward'
+      User.where('name LIKE?', "%#{word}")
+    when 'partial'
+      User.where('name LIKE?', "%#{word}%")
+    end
+  end
 end
